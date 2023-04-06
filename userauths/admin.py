3ad9e6f9e202c77d0eb4ad6c16a16b997232e4ce
AuzCoder <auz.offical@gmail.com>
2023-04-06 18:04:41 +0500

    .

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from userauths.models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email', 'bio', 'date_joined']
+
+admin.site.register(User, UserAdmin)
